0f1effde2c60c3b037710bbbd5eeb14a1fa686c8
Otavio Silva <p_otavio_s@hotmail.com>
2019-10-10 14:19:30 -0300

    -

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-footer',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  template: ``,
 })
 export class FooterComponent {
 }
